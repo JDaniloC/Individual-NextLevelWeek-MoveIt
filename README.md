6887fdc2890c567e448955be9046f26bd10586fc
Danilo Silva <jdsc@cin.ufpe.br>
2021-02-28 18:42:20 -0300

    level up content and readme

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Individual-NextLevelWeek-MoveIt
-A bundle of NextJs application + NodeJs and Elixir teached by Rocketseat
+# Move.it
+<img align="right" src=".github/icon.svg" width="35%" alt="Move.it">
+
+<h4 align="center">
+  🚀 NextLevelWeek #4
+</h4>
+
+<p align="center">
+  <a href="#sobre">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#instalação">Instalação</a>&nbsp;&nbsp;&nbsp;
+</p>
+
+<p align = "center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/jdaniloc/Individual-nextlevelweek-moveit?color=6E40C9&style=flat-square">
+  <img alt="GitHub Top Language" src="https://img.shields.io/github/languages/top/jdaniloc/Individual-nextlevelweek-moveit?color=6E40C9&style=flat-square">
+</p>
+
+## Sobre
+
+O Move.it é um projeto desenvolvido durante a Next Level Week #4 da [Rocketseat](https://rocketseat.com.br/) com o intuito de treinar NextJs e context api do React.
+
+## Tecnologias
+
+Tecnologias e ferramentas utilizadas no desenvolvimento do projeto:
+
+- [React](https://reactjs.org/)
+- [Next.js](https://nextjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+
+![central image](.github/main.png)
+
+
+## Instalação
+
+Certifique-se que você tem o npm instalado, de preferência instale o yarn a partir dele. 
+
+```bash
+# Entre na pasta web com 
+$ cd nextjs
+
+# Instale as dependências
+$ yarn install
+
+# Inicie a aplicação
+$ yarn dev
+```
+
--- nextjs/src/contexts/ChallengeContext.tsx
+++ nextjs/src/contexts/ChallengeContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 import challenges from "../../challenges.json";
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface challenge {
     type: 'body' | 'eye';
@@ -17,21 +19,36 @@ interface ChallengeContextData {
     startNewChallenge: () => void;
     resetChallenge: () => void;
     completeChallenge: () => void;
+    closeLevelModal: () => void;
 }
 
 interface ChallengeProviderProps {
     children: ReactNode;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 }
 
 export const ChallengeContext = createContext({} as ChallengeContextData);
 
-export function ChallengesProvider({ children }: ChallengeProviderProps ) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ 
+        children, ...rest 
+    }: ChallengeProviderProps ) {
+    const [level, setLevel] = useState(rest.level);
+    const [currentExperience, setCurrentExperience] = useState(
+        rest.currentExperience);
+    const [challengesCompleted, setChallengesCompleted] = useState(
+        rest.challengesCompleted);
 
     const [activeChallenge, setActiveChallenge] = useState(null);
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+    const [isLevelModalOpen, setIsLevelModalOpen] = useState(false);
+
+    useEffect(() => {
+        Cookies.set("level", String(level));
+        Cookies.set("currentExperience", String(currentExperience));
+        Cookies.set("challengesCompleted", String(challengesCompleted));
+    }, [level, currentExperience, challengesCompleted]);
 
     useEffect(() => {
         Notification.requestPermission();
@@ -39,6 +56,11 @@ export function ChallengesProvider({ children }: ChallengeProviderProps ) {
 
     function levelUp() {
         setLevel(level + 1);
+        setIsLevelModalOpen(true);
+    }
+
+    function closeLevelModal() {
+        setIsLevelModalOpen(false);
     }
 
     function startNewChallenge() {
@@ -88,8 +110,11 @@ export function ChallengesProvider({ children }: ChallengeProviderProps ) {
             activeChallenge, 
             resetChallenge, levelUp, 
             startNewChallenge, 
-            completeChallenge }}>
+            completeChallenge,
+            closeLevelModal }}>
             {children}
+
+            { isLevelModalOpen && <LevelUpModal /> }
         </ChallengeContext.Provider>
     )
 }
